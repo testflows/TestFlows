920afee2aedc80e85405db9dce96ff2dc05a1028
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-01-06 19:19:35 -0500

    Updating version to 1.3.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.3.7",
+    version="1.3.8",
     description="TestFlows Test Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -35,7 +35,7 @@ setup(
     license="Apache-2.0",
     zip_safe=False,
     install_requires=[
-        "testflows.core>=1.3.191202.1175830",
+        "testflows.core>=1.3.200107.1001713",
         "testflows.asserts>=5.3.191019.1125113",
         "testflows.uexpect>=1.2.191027.1152544",
         "testflows.connect>=1.3.191030.1131916"
