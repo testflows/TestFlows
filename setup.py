69286e602efd5912ea2aa32262fa0f2264755a91
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-03-13 11:27:13 -0400

    Moving to 1.3.48

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.3.47",
+    version="1.3.48",
     description="TestFlows Test Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -35,7 +35,7 @@ setup(
     license="Apache-2.0",
     zip_safe=False,
     install_requires=[
-        "testflows.core>=1.3.200309.1155849",
+        "testflows.core>=1.3.200313.1152559",
         "testflows.asserts>=5.3.191019.1125113",
         "testflows.uexpect>=1.2.191027.1152544",
         "testflows.connect>=1.3.191030.1131916"
