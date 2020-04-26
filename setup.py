ab1dbac0595f15536491fa38d6cede868e656f8a
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-04-26 14:29:19 -0400

    Moving to 1.4.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.4.5",
+    version="1.4.6",
     description="TestFlows Test Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -37,8 +37,8 @@ setup(
     install_requires=[
         "testflows.core>=1.4.200424.1160439",
         "testflows.asserts>=5.3.191019.1125113",
-        "testflows.uexpect>=1.2.191027.1152544",
-        "testflows.connect>=1.3.200421.1170112"
+        "testflows.uexpect>=1.2.200426.1182347",
+        "testflows.connect>=1.3.200426.1182816"
     ],
     extras_require={
         "dev": [
