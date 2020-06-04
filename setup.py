4bbd833ec311a50244743d5e7202852e8203f2cc
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-06-04 18:32:30 -0400

    Moving to 1.5.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.4.6",
+    version="1.5.0",
     description="TestFlows Test Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -36,10 +36,11 @@ setup(
     license="Apache-2.0",
     zip_safe=False,
     install_requires=[
-        "testflows.core>=1.4.200424.1160439",
+        "testflows.core>=1.5.200604.1223022",
         "testflows.asserts>=5.3.191019.1125113",
         "testflows.uexpect>=1.2.200426.1182347",
-        "testflows.connect>=1.3.200426.1182816"
+        "testflows.connect>=1.3.200426.1182816",
+        "testflows.database>=1.3.200604.1222934"
     ],
     extras_require={
         "dev": [
