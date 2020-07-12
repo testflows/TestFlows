88a0582a233fb303f4e352c40e7821734b96aa5b
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-07-12 14:54:07 -0400

    Moving to 1.6.31

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.6.30",
+    version="1.6.31",
     description="TestFlows Test Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
