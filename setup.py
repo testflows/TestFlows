e4bcc5822158a3a013f2ae5a96f6b01f77029972
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-08-17 08:03:27 -0400

    Moving to 1.6.42

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.6.41",
+    version="1.6.42",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -36,7 +36,7 @@ setup(
     license="Apache-2.0",
     zip_safe=False,
     install_requires=[
-        "testflows.core>=1.6.200811.1124123",
+        "testflows.core>=1.6.200817.1120233",
         "testflows.asserts>=6.3.200715.1200940",
         "testflows.uexpect>=1.6.200713.1141539",
         "testflows.connect>=1.6.200715.1200859",
