601f858da6e7f8318f507dc271d9873ea86e3601
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-07-27 22:29:03 -0400

    1.7.16: fixes to async test support and added joining of handlers

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.7.15",
+    version="1.7.16",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -40,7 +40,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "testflows.asyncio==1.1.210505.1133510",
-        "testflows.core==1.7.210726.1032723",
+        "testflows.core==1.7.210728.1022741",
         "testflows.asserts==6.3.210714.1191517",
         "testflows.uexpect==1.6.210510.1163818",
         "testflows.connect==1.7.210510.1164004",
