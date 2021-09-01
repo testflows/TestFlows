374b56b935fa61234fad1e22be17983e0164a4c8
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-09-01 12:06:47 -0400

    1.7.28: fix bug with not running mandatory sub-tests

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.7.27",
+    version="1.7.28",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -40,7 +40,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "testflows.asyncio==1.1.210505.1133510",
-        "testflows.core==1.7.210830.1015416",
+        "testflows.core==1.7.210901.1160511",
         "testflows.asserts==6.3.210831.1135705",
         "testflows.stash==1.1.210901.1113505",
         "testflows.uexpect==1.6.210811.1002209",
