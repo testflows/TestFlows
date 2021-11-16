bb3e291d3df94694a67b935d4f17986742e6a83d
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-11-16 09:41:21 -0500

    1.7.57: adding current_time() function

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.7.56",
+    version="1.7.57",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.7.211116.1140732",
+        "testflows.core==1.7.211116.1144005",
         "testflows.asserts==6.4.211028.1233218",
         "testflows.stash==1.1.210901.1224529",
         "testflows.uexpect==1.6.210811.1002209",
