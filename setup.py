5a0c490a0817cd6a4dc6274060cbbe32b5bd747f
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-12-04 09:31:08 -0500

    1.7.71: adding cleanup() and Cleanup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.7.70",
+    version="1.7.71",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.7.211204.1053229",
+        "testflows.core==1.7.211204.1143017",
         "testflows.asserts==6.4.211028.1233218",
         "testflows.stash==1.1.210901.1224529",
         "testflows.uexpect==1.6.210811.1002209",
