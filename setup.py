b7ed6ef2590eca5e3fc735f726b8257156693982
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-12-08 17:30:10 -0500

    1.7.76: fixing --debug and --no-colors propagation

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.7.75",
+    version="1.7.76",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.7.211208.1212900",
+        "testflows.core==1.7.211208.1222904",
         "testflows.asserts==6.4.211028.1233218",
         "testflows.stash==1.1.210901.1224529",
         "testflows.uexpect==1.6.210811.1002209",
