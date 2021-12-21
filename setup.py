f74f46c6e832980d7c7c92ad89f4a0da1d48f6ec
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-12-21 11:55:34 -0500

    1.7.77: updating testflows.asserts

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.7.76",
+    version="1.7.77",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -40,7 +40,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "testflows.core==1.7.211208.1222904",
-        "testflows.asserts==6.4.211028.1233218",
+        "testflows.asserts==6.4.211221.1165422",
         "testflows.stash==1.1.210901.1224529",
         "testflows.uexpect==1.6.210811.1002209",
         "testflows.connect==1.7.210811.1000931",
