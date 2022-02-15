389b2798fb966516b2e153542aa18c4bf5c9a4e7
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-02-15 09:47:42 -0500

    Moving to 1.8.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.8.01",
+    version="1.8.2",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
