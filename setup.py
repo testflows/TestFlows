3b2bbaae07d550269020a6dc954121b2334ef87b
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-02-23 15:14:03 -0500

    1.8.6: adding support for basic filtering of secret values in logs

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.8.5",
+    version="1.8.6",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.8.220223.1151114",
+        "testflows.core==1.8.220223.1201306",
         "testflows.asserts==6.4.211221.1165422",
         "testflows.stash==1.1.210901.1224529",
         "testflows.uexpect==1.6.210811.1002209",
