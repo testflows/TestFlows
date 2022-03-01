9bfbb39c8e28ac5989b855fafcf74a1f6aded9aa
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-03-01 10:00:37 -0500

    1.8.12: updating how secrets filtering works

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.8.11",
+    version="1.8.12",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.8.220228.1233145",
+        "testflows.core==1.8.220301.1145908",
         "testflows.asserts==6.4.211221.1165422",
         "testflows.stash==1.1.210901.1224529",
         "testflows.uexpect==1.6.210811.1002209",
