2c15f724ebad0907ee46a26f78218acb50c43c14
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-05-10 12:05:57 -0400

    1.9.1: adding support for process pool executor; updating API to return result object and not test.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.8.22",
+    version="1.9.1",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,9 +39,9 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.8.220329.1183501",
+        "testflows.core==1.9.220510.1160438",
         "testflows.asserts==6.4.211221.1165422",
-        "testflows.stash==1.1.210901.1224529",
+        "testflows.stash==1.1.220510.1154615",
         "testflows.uexpect==1.6.210811.1002209",
         "testflows.connect==1.7.210811.1000931",
         "testflows.database==1.6.200713.1142213"
