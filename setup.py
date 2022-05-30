a84e932783244d6ba422c7462b5ef9616fb96d94
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-05-30 16:11:51 -0400

    1.9.5: updates to handling retries

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.9.4",
+    version="1.9.5",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.9.220524.1140201",
+        "testflows.core==1.9.220530.1201050",
         "testflows.asserts==6.4.211221.1165422",
         "testflows.stash==1.1.220510.1154615",
         "testflows.uexpect==1.6.210811.1002209",
