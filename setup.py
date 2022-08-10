5ac0947bc0a6b89e0c06fe7eebb3679fa2b036a2
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-08-10 15:25:56 -0400

    1.9.15: adding support for 'when' condition for xflags and xfails

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.9.14",
+    version="1.9.15",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.9.220809.1232310",
+        "testflows.core==1.9.220810.1192506",
         "testflows.asserts==6.4.211221.1165422",
         "testflows.stash==1.1.220510.1154615",
         "testflows.uexpect==1.6.210811.1002209",
