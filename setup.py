b405b0ecc6720ebdc2a1e2268f0a7a958716fb6d
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-10-24 09:30:24 -0400

    1.9.26: fixing service default ip address

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.9.25",
+    version="1.9.26",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.9.221022.1162344",
+        "testflows.core==1.9.221024.1132612",
         "testflows.asserts==6.5.221019.1152644",
         "testflows.stash==1.1.220510.1154615",
         "testflows.uexpect==1.7.220902.1112725",
