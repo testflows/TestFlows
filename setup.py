40568c517398916fc5d41e737f6dacdef481bd9b
vzakaznikov <vzakaznikov@protonmail.com>
2023-03-01 19:37:13 -0500

    1.9.51: adding support for result_message re expression in xfails.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.9.50",
+    version="1.9.51",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.9.230213.1152142",
+        "testflows.core==1.9.230302.1003619",
         "testflows.asserts==6.5.221019.1152644",
         "testflows.stash==1.1.220510.1154615",
         "testflows.uexpect==1.7.220902.1112725",
