8a0dac8f125f265df178bc3519766c2246e50c6b
vzakaznikov <vzakaznikov@protonmail.com>
2023-03-14 20:32:05 -0400

    1.9.52: fixing bug with tfs --trim-results

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="testflows",
-    version="1.9.51",
+    version="1.9.52",
     description="TestFlows.com Open-Source Software Testing Framework",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
@@ -39,7 +39,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        "testflows.core==1.9.230302.1003619",
+        "testflows.core==1.9.230315.1003122",
         "testflows.asserts==6.5.221019.1152644",
         "testflows.stash==1.1.220510.1154615",
         "testflows.uexpect==1.7.220902.1112725",
